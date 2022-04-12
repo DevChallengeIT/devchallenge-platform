526d2adffe9109a8b9a1f40d1bc5ed3d2ada8fc0
Andrii Pyrin <andriy.pyrin@teamvoy.com>
2022-04-12 21:16:51 +0300

    add tasks create action

--- app/controllers/admin/tasks_controller.rb
+++ app/controllers/admin/tasks_controller.rb
@@ -9,5 +9,22 @@ module Admin
     def new
       @task = Repo::Task.new
     end
+
+    def create
+      @task = Repo::Task.new(task_params)
+
+      if task.save
+        redirect_to(edit_admin_task_path(task), notice: flash_message(:created, :tasks))
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def task_params
+      params.require(:task).permit(:title, :description, :slug, :registration_at, :challenge_id,
+                                   :start_at, :submit_at, :result_at, :dependent_task_id, :min_assestment)
+    end
   end
 end
