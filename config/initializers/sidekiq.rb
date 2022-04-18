8440d7cc0746466a7ad41a1565efa9a784cc04db
Bohdan Lomey <blomey@intelliarts.com>
2022-04-18 21:18:03 +0300

    Fix rubocop offenses

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,6 @@
-sidekiq_config = { url: Rails.application.credentials.job_worker_url }.freeze
+# frozen_string_literal: true
+
+sidekiq_config = { url: Rails.application.credentials.job_worker_url }
 
 Sidekiq.configure_server do |config|
   config.redis = sidekiq_config
