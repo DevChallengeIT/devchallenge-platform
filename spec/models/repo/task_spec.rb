174af6c46e51c304b165a1223cc000b55b5fe20f
Andrii Pyrin <andriy.pyrin@teamvoy.com>
2022-04-19 10:17:57 +0300

    add associations test to task

--- spec/models/repo/task_spec.rb
+++ spec/models/repo/task_spec.rb
@@ -5,6 +5,12 @@ require 'rails_helper'
 RSpec.describe Repo::Task, type: :model do
   subject { create(:task) }
 
+  describe 'associations' do
+    it { is_expected.to belong_to(:challenge) }
+    it { is_expected.to belong_to(:dependent_task).optional }
+    it { is_expected.to have_many(:task_submissions) }
+  end
+
   describe 'validations' do
     it { is_expected.to validate_presence_of(:title) }
     it { is_expected.to validate_uniqueness_of(:title).scoped_to(:challenge_id).case_insensitive }
