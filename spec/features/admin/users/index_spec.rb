978be522bba64c57274ff6089b2e3e6129e698cf
Andrii Yaremchuk <jaremchuk.andrij@gmail.com>
2022-04-21 14:52:18 +0300

    Add test fixes

--- spec/features/admin/users/index_spec.rb
+++ spec/features/admin/users/index_spec.rb
@@ -29,35 +29,34 @@ RSpec.describe 'Admin/Users/Index' do
       expect(page).to have_content user.full_name
       expect(page).to have_content user.slug
       expect(page).to have_content user.time_zone
-      expect(page).to have_content user.reset_password_sent_at
       expect(page).to have_content user.sign_in_count
-      expect(page).to have_content user.current_sign_in_ip
-      expect(page).to have_content user.last_sign_in_ip
     end
   end
 
-  it 'handles pagination' do
-    user_a = create(:user)
-    user_b = create(:user)
+  context 'with pagination' do
+    let!(:user_a) { create(:user) }
+    let!(:user_b) { create(:user) }
 
-    assume_logged_in(admin: true)
-    visit '/admin/users?per_page=1'
+    it 'handles pagination' do
+      assume_logged_in(admin: true)
+      visit '/admin/users?per_page=1'
 
-    within '.pagination' do
-      expect(page).to have_css "span[class='page active']", text: '1'
-      expect(page).to have_css "a[href='/admin/users?per_page=1&page=2']", text: '2'
-      expect(page).to have_css "a[href='/admin/users?per_page=1&page=2']", text: 'Next'
+      within '.pagination' do
+        expect(page).to have_css "span[class='page active']", text: '1'
+        expect(page).to have_css "a[href='/admin/users?per_page=1&page=2']", text: '2'
+        expect(page).to have_css "a[href='/admin/users?per_page=1&page=2']", text: 'Next'
+      end
+
+      expect(page).to have_css "#user-#{user_a.id}"
+      expect(page).not_to have_css "#user-#{user_b.id}"
+
+      within '.pagination' do
+        click_link 'Next'
+        expect(page).to have_current_path '/admin/users?per_page=1&page=2'
+      end
+
+      expect(page).to have_css "#user-#{user_b.id}"
+      expect(page).not_to have_css "#user-#{user_a.id}"
     end
-
-    expect(page).to have_css "#user-#{user_a.id}"
-    expect(page).not_to have_css "#user-#{user_b.id}"
-
-    within '.pagination' do
-      click_link 'Next'
-      expect(page).to have_current_path '/admin/users?per_page=1&page=2'
-    end
-
-    expect(page).to have_css "#user-#{user_b.id}"
-    expect(page).not_to have_css "#user-#{user_a.id}"
   end
 end
