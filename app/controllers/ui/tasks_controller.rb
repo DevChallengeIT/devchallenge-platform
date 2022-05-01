0b351b14b1728ceef0e4d8b5720f94939d1fa1c4
Andrii Pyrin <pyrin.andrii@gmail.com>
2022-05-01 14:14:53 +0300

    show judges a list of submissions for task

--- app/controllers/ui/tasks_controller.rb
+++ app/controllers/ui/tasks_controller.rb
@@ -5,7 +5,16 @@ module UI
     include CompetitionContext
 
     before_action :authenticate_user!, :authorize_member!
-    helper_method :task, :task_submission
+    helper_method :task, :task_submission, :task_submissions
+
+    def show
+      if current_member&.judge?
+        @paginator, @task_submissions = paginate task_submissions
+        render 'ui/tasks/judges/show'
+      else
+        super
+      end
+    end
 
     private
 
@@ -14,11 +23,19 @@ module UI
     end
 
     def task
-      @task ||= Repo::Task.preload(:challenge, :rich_text_description, :task_submissions).friendly.find(params[:id])
+      @task ||= Repo::Task.preload(
+        :challenge,
+        :rich_text_description,
+        :task_submissions
+      ).friendly.find(params[:id])
     end
 
     def task_submission
       @task_submission ||= task.task_submissions.find_by(member: current_member) || Repo::TaskSubmission.new
+    end
+
+    def task_submissions
+      @task_submissions ||= task.task_submissions.preload(:task_assessments)
     end
   end
 end
