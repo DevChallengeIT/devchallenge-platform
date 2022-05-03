2bd9848b6285934b1df426bcbaca8fdef6c7c800
Andrii Pyrin <pyrin.andrii@gmail.com>
2022-05-03 20:18:43 +0300

    add update assessment

---
+++ app/controllers/ui/assessments_controller.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+module UI
+  class AssessmentsController < BaseController
+    include CompetitionContext
+
+    helper_method :task, :task_submission, :task_criteria, :task_assessments
+
+    before_action :authenticate_user!
+
+    def edit
+      @task_assessment = task_submission.task_assessments.first
+    end
+
+    def update
+      @task_submission = task_assessment.task_submission
+      if @task_assessment.update(task_assessment_params)
+        redirect_to task_path(task), notice: flash_message(:updated, :task_assessments)
+      else
+        redirect_to task_path(task), status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def task
+      @task ||= task_submission.task
+    end
+
+    def task_criteria
+      @task_criteria ||= task.task_criteria
+    end
+
+    def task_assessments
+      @task_assessments ||= task_submission.task_assessments
+    end
+
+    def task_assessment
+      @task_assessment ||= Repo::TaskAssessment.find(params[:id])
+    end
+
+    def challenge
+      @challenge ||= task.challenge
+    end
+
+    def task_submission
+      @task_submission ||= Repo::TaskSubmission.preload(:task_assessments, task: [:challenge, :task_criteria]).find(params[:task_submission])
+    end
+
+    def task_assessment_params
+      params.require(:task_assessment).permit(:value, :comment)
+    end
+  end
+end
