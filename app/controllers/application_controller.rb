98b808e7b1fedfc7e4d2126abff2a7a2dc1cba80
Volodya Sveredyuk <sveredyuk@gmail.com>
2022-09-06 13:39:44 +0300

    Add respond_to :html, :json in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
 
+  respond_to :html, :json
+
   protect_from_forgery with: :null_session, prepend: true
 
   before_action :configure_permitted_parameters, if: :devise_controller?
