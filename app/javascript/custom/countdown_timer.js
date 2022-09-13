aa4f16649f7ea82f8a3d8422daa7573f98d70027
Bohdan Lomey <blomey@intelliarts.com>
2022-09-13 13:19:50 +0300

    Fix jumping of timer

--- app/javascript/custom/countdown_timer.js
+++ app/javascript/custom/countdown_timer.js
@@ -2,7 +2,7 @@ const dayInMsec = 1000 * 60 * 60 * 24;
 const hourInMsec = 1000 * 60 * 60;
 const minuteInMsec = 1000 * 60;
 
-function updateTimer(id, millisecondsLeft) {
+function calcTimer(millisecondsLeft) {
   const msecLeft = millisecondsLeft < 1000 ? 0 : millisecondsLeft;
 
   // Time calculations for days, hours, minutes and seconds
@@ -11,20 +11,36 @@ function updateTimer(id, millisecondsLeft) {
   const minutes = Math.floor((msecLeft % hourInMsec) / minuteInMsec);
   const seconds = Math.floor((msecLeft % minuteInMsec) / 1000);
 
-  document.getElementById(id).innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+  return days + "d " + hours + "h " + minutes + "m " + seconds + "s";
 }
 
 function initializeTimer(id, initialMsecLeft) {
   let msecLeft = initialMsecLeft;
-  updateTimer(id, msecLeft);
+  let timerEl = document.getElementById(id);
+  timerEl.innerHTML = calcTimer(msecLeft);
 
-  const x = setInterval(() => {
-    updateTimer(id, msecLeft -= 1000);
+  const intervalId = setInterval(() => {
+    timerEl = document.getElementById(id);
+    if (!timerEl) {
+      clearInterval(intervalId);
+      return;
+    }
+
+    const currIntervalId = parseInt(timerEl.getAttribute('interval-id'));
+    if (currIntervalId && currIntervalId !== intervalId) {
+      clearInterval(intervalId);
+      return;
+    }
 
     if (msecLeft < 1000) {
-      clearInterval(x);
+      clearInterval(intervalId);
+      return;
     }
+
+    timerEl.innerHTML = calcTimer(msecLeft -= 1000);
   }, 1000);
+
+  timerEl.setAttribute('interval-id', intervalId.toString());
 }
 
 window.initializeTimer = initializeTimer;
